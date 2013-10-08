d059aaebcfcb33da9bf6d60095bef4418d220a48
aljane.jamel@gmail.com <aljane.jamel@gmail.com@ea14c930-9557-c7cb-dc0a-7624f185a2d5>
2013-10-08 04:16:34 +0000

    first commits,remodeling  and refactoring... some slices missing.

---
+++ src/java/org/proctosequel/runtime/QueryEval.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.proctosequel.runtime;
+
+/**
+ *
+ * @author Jamel Aljane <aljane.jamel@gmail.com>
+ */
+public class QueryEval {
+
+}
