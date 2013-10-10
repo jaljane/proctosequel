bdf5c5fa93b04306b715157c7b4137fdacb7cdb1
aljane.jamel@gmail.com <aljane.jamel@gmail.com@ea14c930-9557-c7cb-dc0a-7624f185a2d5>
2013-10-10 03:42:19 +0000

    first commits,remodeling  and refactoring... some slices missing.

--- src/java/org/proctosequel/parsing/utils/Constants.java
+++ src/java/org/proctosequel/parsing/utils/Constants.java
@@ -6,5 +6,6 @@ package org.proctosequel.parsing.utils;
  * @author Jamel Aljane <aljane.jamel@gmail.com>
  */
 public class Constants {
-    public static String VAR_NAME_REGEX = "\\$[a-zA-z0-9_\\.]+";
+    public static String VAR_NAME_REGEX = "\\$[a-zA-Z0-9_\\.]+";
+    public static String WORD_REGEX = "[a-zA-Z0-9_]+";
 }
