ea021ecd3a5a6ea473248907538a4f28c5b51ee7
aljane.jamel@gmail.com <aljane.jamel@gmail.com@ea14c930-9557-c7cb-dc0a-7624f185a2d5>
2013-10-23 01:46:23 +0000

    first commits , some slices missing.

--- src/java/org/proctosequel/parsing/visitors/composite/IsolateOrExprVisitor.java
+++ src/java/org/proctosequel/parsing/visitors/composite/IsolateOrExprVisitor.java
@@ -5,9 +5,7 @@
 package org.proctosequel.parsing.visitors.composite;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.proctosequel.antlr.LogicExprGrammarParser;
@@ -20,14 +18,14 @@ import org.proctosequel.parsing.utils.ProctosequelHelper;
  */
 public class IsolateOrExprVisitor  extends ProcToSequelGrammarBaseVisitor {
 
-    private List<LogicExprGrammarParser.SubCondContext> orExprs = new ArrayList<>();
+    private List<ParseTree> orExprs = new ArrayList<>();
 
     @Override
     public Object visitTerminal(TerminalNode tn) {
         if("or".equals(tn.getText())){
-            LogicExprGrammarParser.SubCondContext subCondContext = getParentToIsolate(tn);
+            ParseTree subCondContext = getParentToIsolate(tn);
             if(!orExprs.contains(subCondContext) ){
-                getOrExprs().add(subCondContext);
+                orExprs.add(subCondContext);
             }
             
         }
@@ -38,11 +36,11 @@ public class IsolateOrExprVisitor  extends ProcToSequelGrammarBaseVisitor {
     /**
      * @return the orExprs
      */
-    public List<LogicExprGrammarParser.SubCondContext> getOrExprs() {
-        List<LogicExprGrammarParser.SubCondContext> filtred = new ArrayList<>();
-        for(LogicExprGrammarParser.SubCondContext subCondContext1 : orExprs){
+    public List<ParseTree> getOrExprs() {
+        List<ParseTree> filtred = new ArrayList<>();
+        for(ParseTree subCondContext1 : orExprs){
             boolean accept = true;
-            for(LogicExprGrammarParser.SubCondContext subCondContext2 : orExprs){
+            for(ParseTree subCondContext2 : orExprs){
                 if(ProctosequelHelper.childOf(subCondContext1, subCondContext2)){
                     accept = false;
                     break;
@@ -55,16 +53,17 @@ public class IsolateOrExprVisitor  extends ProcToSequelGrammarBaseVisitor {
         return filtred;
     }
 
-    private LogicExprGrammarParser.SubCondContext getParentToIsolate(ParseTree parseTree){
+    private ParseTree getParentToIsolate(ParseTree parseTree){
         if(parseTree.getParent()==null){
-            return (LogicExprGrammarParser.SubCondContext) parseTree;
+            return parseTree;
         }
         if(parseTree.getParent().getText().startsWith("(") 
-                && parseTree.getParent() instanceof LogicExprGrammarParser.SubCondContext){
-            return (LogicExprGrammarParser.SubCondContext)parseTree.getParent();
+                && (parseTree.getParent() instanceof LogicExprGrammarParser.SubCondContext
+                || parseTree.getParent() instanceof LogicExprGrammarParser.ConditionContext)){
+            return parseTree.getParent();
         }
         if(parseTree.getParent() == null){
-            return (LogicExprGrammarParser.SubCondContext) parseTree;
+            return parseTree;
         }
         return getParentToIsolate(parseTree.getParent());
         
